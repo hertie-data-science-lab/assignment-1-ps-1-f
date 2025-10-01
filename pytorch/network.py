2e40f06606744a9304de2463651c8313b51f7836
Franco Bastida <42223533+francobastida@users.noreply.github.com>
2025-10-02 01:19:41 +0200

    Implement for fwd, bwd, update, predict

--- pytorch/network.py
+++ pytorch/network.py
@@ -4,6 +4,18 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 
+'''
+Problem set up: We have images in greyscale that have a pixel resolution of 28x28.
+These can be a number from 0 to 9, this means K = 10 for the classification. 
+Since our neural network (NN) expects a 1D vector, not a matrix, by flattening the inputs, we would get 28 x 28 = 784 features.
+
+Input of first layer: 784 neurons, 1 per pixel.
+Output before softmax: 10 scores.
+Output after softmax: 10 numbers (probabilities) that sum to 1.
+
+Based on d2l.ai reference, all probabilities must must be greater or equal to 0 and must sum to 1. 
+Softmax allows us to transform logits into a probability distribution p_k = 1.
+'''
 
 class TorchNetwork(nn.Module):
     def __init__(self, sizes, epochs=10, learning_rate=0.01, random_state=1):
@@ -19,38 +31,59 @@ class TorchNetwork(nn.Module):
         self.linear2 = nn.Linear(sizes[1], sizes[2])
         self.linear3 = nn.Linear(sizes[2], sizes[3])
 
+        '''
+        Our activations
+        '''
         self.activation_func = torch.sigmoid
         self.output_func = torch.softmax
-        self.loss_func = nn.BCEWithLogitsLoss()
+
+        '''
+        Mean Square Loss
+        '''
+        self.loss_func = nn.MSELoss() #as specificed in the description
+
         self.optimizer = optim.SGD(self.parameters(), lr=learning_rate)
 
-
-
+    '''
+    Forward Pass
+    Implementing the forward propagation algorithm to return the output of the network.
+    '''
     def _forward_pass(self, x_train):
-        '''
-        TODO: Implement the forward propagation algorithm.
-        The method should return the output of the network.
-        '''
-        pass
+        #First hidden layer
+        h1 = self.activation_func(self.linear1(x_train))
+        #Second hidden layer
+        h2 = self.activation_func(self.linear2(h1))
+        #Output logits (raw scores for 10 classes)
+        outlogit = self.linear3(h2)
+        #Softmax to get them to 1, 1 per row (dim)
+        probabilities = self.output_func(outlogit, dim=1)
+        return probabilities
 
+    '''
+    Backward Pass
+    Implementing the forward propagation algorithm to return the output of the network.
+    '''
 
     def _backward_pass(self, y_train, output):
-        '''
-        TODO: Implement the backpropagation algorithm responsible for updating the weights of the neural network.
-        '''
-        pass
+        #We use float instead of string, so they are not treated as integers, outputs are from the probabilities
+        y_train = y_train.float()
+        #Computing the MSE Loss
+        loss = self.loss_func(output, y_train)
+        #Computing all the gradients that are stored in each tensor
+        loss.backward()
+        #Stores the float from the tensor
+        return loss.item()
 
-
+    '''
+    Weights
+    Updating the network of weights per SDG.
+    '''
     def _update_weights(self):
-        '''
-        TODO: Update the network weights according to stochastic gradient descent.
-        '''
-        pass
-
+        #PyTorch loop looks at parameters' gradient and updates to change weights
+        self.optimizer.step()
 
     def _flatten(self, x):
         return x.view(x.size(0), -1)       
-
 
     def _print_learning_progress(self, start_time, iteration, train_loader, val_loader):
         train_accuracy = self.compute_accuracy(train_loader)
@@ -63,15 +96,17 @@ class TorchNetwork(nn.Module):
             f'Validation Accuracy: {val_accuracy * 100:.2f}%'
             )
 
-
+    '''
+    Predict
+    To return the most likely output class
+    '''
     def predict(self, x):
-        '''
-        TODO: Implement the prediction making of the network.
-
-        The method should return the index of the most likeliest output class.
-        '''
-        pass
-
+        #Flatten the input to compare true values into 784 features
+        x = self._flatten(x)
+        with torch.no_grad():
+            prob = self._forward_pass(x)
+        predictions = torch.argmax(prob)
+        return predictions
 
     def fit(self, train_loader, val_loader):
         start_time = time.time()
@@ -88,9 +123,6 @@ class TorchNetwork(nn.Module):
                 self._update_weights()
 
             self._print_learning_progress(start_time, iteration, train_loader, val_loader)
-
-
-
 
     def compute_accuracy(self, data_loader):
         correct = 0
