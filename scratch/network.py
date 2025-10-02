89f48eb768804f89b0c7d7348f5e80d91c504712
lonnychen <lonny.cl.chen@gmail.com>
2025-10-02 22:32:23 +0200

    Complete backprop and update methods (yet to test!)

--- scratch/network.py
+++ scratch/network.py
@@ -20,6 +20,14 @@ class Network():
 
         self.params = self._initialize_weights()
 
+        #ADD: need to cache hidden layer inputs and outputs for backprop
+        self.a3 = np.zeros(self.sizes[3])
+        self.h2 = np.zeros(self.sizes[2])
+        self.a2 = np.zeros(self.sizes[2])
+        self.h1 = np.zeros(self.sizes[1])
+        self.a1 = np.zeros(self.sizes[1])
+        self.x = np.zeros(self.sizes[0])
+
 
     def _initialize_weights(self):
         # number of neurons in each layer
@@ -44,35 +52,66 @@ class Network():
         DONE: Implement the forward propagation algorithm.
         The method should return the output of the network.
         '''
+        # Save x_train for backprop
+        self.x = x_train
+        
         # 1. Input to hidden 1
-        a1 = self.params['W1'] @ x_train #weighted sum = W1@X (no bias!)
-        h1 = self.activation_func(a1) #sigmoid activation: h1i ∈ (0,1)
+        self.a1 = self.params['W1'] @ x_train #weighted sum = W1@X (no bias!)
+        self.h1 = self.activation_func(self.a1) #sigmoid activation: h1i ∈ (0,1)
 
         # 2. Hidden 1 to hidden 2
-        a2 = self.params['W2'] @ h1 #weighted sum
-        h2 = self.activation_func(a2) #sigmoid
+        self.a2 = self.params['W2'] @ self.h1 #weighted sum
+        self.h2 = self.activation_func(self.a2) #sigmoid
 
         # 3. Hidden 2 to output activation
-        a3 = self.params['W3'] @ a2 #weighted sum
-        output = self.output_func(a3) #softmax activation: sum(output) = 1.0
+        self.a3 = self.params['W3'] @ self.h2 #weighted sum
+        output = self.output_func(self.a3) #softmax output: sum(output) = 1.0
 
         return output
 
 
     def _backward_pass(self, y_train, output):
         '''
-        TODO: Implement the backpropagation algorithm responsible for updating the weights of the neural network.
+        DONE: Implement the backpropagation algorithm responsible for updating the weights of the neural network.
         The method should return a dictionary of the weight gradients which are used to update the weights in self._update_weights().
         '''  
-       
-        pass
+        # Count backwards! all derivative variables are dL/...
+        
+        # 3. Loss to output to hidden 2 weights
+        dy = self.cost_func_deriv(y_train, output) # (sizes[3],) #mse loss
+        da3 = dy * self.output_func_deriv(self.a3) # (sizes[3],) #softmax output
+        dw3 = da3[:, np.newaxis] @ self.h2[np.newaxis, :] # (sizes[3], sizes[2]) #weighted sum
+        
+        # 2. Hidden 2 to hidden 1 weights
+        dh2 = da3 @ self.params['W3'] # (sizes[2],) #weighted sum
+        da2 = dh2 * self.activation_func_deriv(self.a2) # (sizes[2],) #sigmoid
+        dw2 = da2[:, np.newaxis] @ self.h1[np.newaxis, :] # (sizes[2], sizes[1]) #weighted sum
+
+        # 1. Hidden 1 to input weights
+        dh1 = da2 @ self.params['W2'] # (sizes[1],) #weighted sum
+        da1 = dh1 * self.activation_func_deriv(self.a1) # (sizes[1],) #sigmoid
+        dw1 = da1[:, np.newaxis] @ self.x[np.newaxis, :] # (sizes[1], sizes[0]) #weighted sum
+
+        # Create gradient: all of the partial derivatives to update weights
+        weights_gradient = {
+            'dW1': dw1,
+            'dW2': dw2,
+            'dW3': dw3,
+        }
+        
+        return weights_gradient
 
 
     def _update_weights(self, weights_gradient, learning_rate):
         '''
-        TODO: Update the network weights according to stochastic gradient descent.
+        DONE: Update the network weights according to stochastic gradient descent.
         '''
-        pass
+        # Update each set of weights by its negative gradient * learning_rate
+        self.params['W1'] -= learning_rate * weights_gradient['dW1']
+        self.params['W2'] -= learning_rate * weights_gradient['dW2']
+        self.params['W3'] -= learning_rate * weights_gradient['dW3']
+        
+        return None
 
 
     def _print_learning_progress(self, start_time, iteration, x_train, y_train, x_val, y_val):
@@ -102,8 +141,9 @@ class Network():
         '''
         #run forward pass to get the softmax output
         output = self._forward_pass(x)
-        
-        return np.argmax(output) #output "prediction" (returns only first index in case of ties!)
+
+        #output "prediction" (returns only first index of largest value in case of ties!)
+        return np.argmax(output)
 
     def fit(self, x_train, y_train, x_val, y_val, cosine_annealing_lr=False):
 
